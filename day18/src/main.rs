fbb899d9bf1f0c7b399d9f3134b8f76331b5ac80
Filip Soukup <fsoukup@artex-is.cz>
2023-12-18 23:39:26 +0100

    day18 - part 2 - pre cleanup

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, env, fs, thread::panicking, vec};
+use std::{collections::HashMap, env, fs, vec};
 
 fn main() {
     let mut args = env::args();
@@ -23,7 +23,7 @@ fn main() {
 
     let (mut current_row, mut current_col) = (0, 0);
 
-    let mut range_map: HashMap<i32, Vec<(i32, i32)>> = HashMap::new();
+    let mut range_map: HashMap<i64, Vec<(i64, i64)>> = HashMap::new();
 
     for Instruction {
         direction,
@@ -59,12 +59,60 @@ fn main() {
         }
     }
 
+    let part_1_result = solve(&range_map);
+
+    println!("Part 1 result {part_1_result}");
+
+    let instructions: Vec<_> = instructions
+        .iter()
+        .map(|e| FixedInstruction::from(e))
+        .collect();
+
+    let (mut current_row, mut current_col) = (0, 0);
+
+    let mut range_map: HashMap<i64, Vec<(i64, i64)>> = HashMap::new();
+
+    for FixedInstruction { direction, steps } in &instructions {
+        match direction {
+            Direction::Up => {
+                current_row -= steps;
+            }
+            Direction::Right => {
+                let range = (current_col, current_col + steps);
+                if let Some(ranges) = range_map.get_mut(&current_row) {
+                    ranges.push(range);
+                } else {
+                    range_map.insert(current_row, vec![range]);
+                }
+                current_col += steps;
+            }
+            Direction::Down => {
+                current_row += steps;
+            }
+            Direction::Left => {
+                let range = (current_col - steps, current_col);
+                if let Some(ranges) = range_map.get_mut(&current_row) {
+                    ranges.push(range);
+                } else {
+                    range_map.insert(current_row, vec![range]);
+                }
+                current_col -= steps;
+            }
+        }
+    }
+
+    let part_2_result = solve(&range_map);
+
+    println!("Part 2 result {part_2_result}");
+}
+
+fn solve(range_map: &HashMap<i64, Vec<(i64, i64)>>) -> i64 {
     let mut rows: Vec<_> = range_map.keys().collect();
     rows.sort();
 
-    let mut open_ranges: Vec<(i32, i32)> = vec![];
+    let mut open_ranges: Vec<(i64, i64)> = vec![];
     let mut last_row = 0;
-    let mut part_1_result = 0;
+    let mut result = 0;
 
     for row in rows {
         let mut ranges = range_map[row].clone();
@@ -78,7 +126,7 @@ fn main() {
         }
 
         for (start, end) in &open_ranges {
-            part_1_result += ((end - start).abs() + 1) * (row - last_row).abs();
+            result += ((end - start).abs() + 1) * (row - last_row).abs();
         }
 
         let mut next_open_ranges = vec![];
@@ -95,20 +143,20 @@ fn main() {
                 open_ranges[0] = (first_range.0, first_open_range.1);
             } else if first_range.0 == first_open_range.0 {
                 ranges.remove(0);
-                part_1_result += (first_range.1 - first_range.0).abs();
+                result += (first_range.1 - first_range.0).abs();
 
                 if first_range.1 == first_open_range.1 {
-                    part_1_result += 1;
+                    result += 1;
                     open_ranges.remove(0);
                 } else {
                     open_ranges[0] = (first_range.1, first_open_range.1);
                 }
             } else if first_range.1 == first_open_range.1 {
                 ranges.remove(0);
-                part_1_result += (first_range.1 - first_range.0).abs();
+                result += (first_range.1 - first_range.0).abs();
 
                 if first_range.0 == first_open_range.0 {
-                    part_1_result += 1;
+                    result += 1;
                     open_ranges.remove(0);
                 } else {
                     open_ranges[0] = (first_open_range.0, first_range.0);
@@ -123,7 +171,7 @@ fn main() {
                 ranges.remove(0);
 
                 next_open_ranges.push((first_open_range.0, first_range.0));
-                part_1_result += (first_range.1 - first_range.0).abs() - 1;
+                result += (first_range.1 - first_range.0).abs() - 1;
 
                 open_ranges[0] = (first_range.1, first_open_range.1);
             } else {
@@ -138,13 +186,13 @@ fn main() {
         last_row = *row;
     }
 
-    println!("Part 1 result {part_1_result}");
+    result
 }
 
 #[derive(Debug)]
 struct Instruction {
     direction: Direction,
-    steps: i32,
+    steps: i64,
     color: String,
 }
 
@@ -154,7 +202,7 @@ impl Instruction {
             direction: direction.into(),
             steps: steps
                 .parse()
-                .expect(&format!("Steps {steps} must be a valid i32")),
+                .expect(&format!("Steps {steps} must be a valid i64")),
             color: color.into(),
         }
     }
@@ -176,6 +224,39 @@ impl From<&str> for Direction {
             "D" => Direction::Down,
             "L" => Direction::Left,
             _ => panic!("Unexpected direction {value}"),
+        }
+    }
+}
+
+impl From<Option<char>> for Direction {
+    fn from(value: Option<char>) -> Self {
+        match value {
+            Some('0') => Direction::Right,
+            Some('1') => Direction::Down,
+            Some('2') => Direction::Left,
+            Some('3') => Direction::Up,
+            _ => panic!("Unexpected direction {value:?}"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct FixedInstruction {
+    direction: Direction,
+    steps: i64,
+}
+
+impl FixedInstruction {
+    fn from(instruction: &Instruction) -> Self {
+        let steps: String = instruction.color.chars().skip(2).take(5).collect();
+        let steps =
+            i64::from_str_radix(steps.as_str(), 16).expect("Color is not a valid hex string");
+
+        let direction = instruction.color.chars().skip(7).next();
+
+        Self {
+            direction: direction.into(),
+            steps,
         }
     }
 }
