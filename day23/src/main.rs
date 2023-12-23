00afd2ecfba7a657ebd9c7b5447a9331b6c5c8ed
Filip Soukup <fsoukup@artex-is.cz>
2023-12-24 00:00:00 +0100

    day23 - part 2

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,7 +1,6 @@
 use std::{
     collections::{HashMap, HashSet},
-    env, fs,
-    io::Empty,
+    env, fs, vec,
 };
 
 fn main() {
@@ -35,17 +34,31 @@ fn main() {
         .expect("End not found")
         .expect("End not found");
 
-    let mut visited: HashSet<Position> = HashSet::new();
-    let part_1_result = find_longest_path(&map, &(map.len() - 1, end), (0, start), &mut visited);
+    let visited: HashSet<Position> = HashSet::new();
+    let part_1_result = find_longest_path(&map, &(map.len() - 1, end), (0, start), visited);
 
     println!("Part 1 result {part_1_result:?}");
+
+    let mut graph = HashMap::new();
+    graph.insert((0, start), vec![]);
+
+    build_graph(&map, &(map.len() - 1, end), &(0, start), &mut graph);
+
+    let visited: HashSet<Position> = HashSet::new();
+    let part_2_result = find_longest_graph_path(&graph, &(map.len() - 1, end), (0, start), visited);
+
+    println!("Part 2 result {part_2_result:?}");
 }
+
+type Map = Vec<Vec<Node>>;
+type Position = (usize, usize);
+type Graph = HashMap<Position, Vec<(Position, usize)>>;
 
 fn find_longest_path(
     map: &Map,
     end: &Position,
     position: Position,
-    visited: &mut HashSet<Position>,
+    mut visited: HashSet<Position>,
 ) -> Option<usize> {
     if position.0 == end.0 && position.1 == end.1 {
         return Some(0);
@@ -53,27 +66,116 @@ fn find_longest_path(
 
     visited.insert(position);
 
-    let next_steps = next_steps(map, &position, visited);
+    let next_steps = next_steps(map, &position, &visited, true);
 
     if next_steps.len() == 1 {
-        let position = *next_steps.first().unwrap();
+        let &(_, position) = next_steps.first().unwrap();
         return find_longest_path(map, end, position, visited).map(|e| e + 1);
     }
 
     next_steps
         .into_iter()
-        .filter_map(|e| {
-            let mut visited = visited.clone();
-            find_longest_path(map, end, e, &mut visited)
+        .filter_map(|(_, position)| {
+            let visited = visited.clone();
+            find_longest_path(map, end, position, visited)
         })
         .max()
         .map(|e| e + 1)
 }
 
-type Map = Vec<Vec<Node>>;
-type Position = (usize, usize);
+fn build_graph(map: &Map, end: &Position, start: &Position, graph: &mut Graph) {
+    let mut queue = vec![start.clone()];
+    graph.insert(start.clone(), vec![]);
+    let mut visited = HashSet::new();
+    visited.insert(start.clone());
 
-fn next_steps(map: &Map, position: &Position, visited: &HashSet<Position>) -> Vec<Position> {
+    while let Some(junction) = queue.pop() {
+        visited.insert(junction);
+
+        for (_, position) in next_steps(map, &junction, &visited, false) {
+            if let Some((next_junction, distance)) =
+                get_next_junction(map, end, position, 1, &mut visited, graph)
+            {
+                queue.push(next_junction);
+                if let Some(neighbors) = graph.get_mut(&next_junction) {
+                    neighbors.push((junction, distance));
+                } else {
+                    graph.insert(next_junction, vec![(junction, distance)]);
+                }
+
+                graph
+                    .get_mut(&junction)
+                    .unwrap()
+                    .push((next_junction, distance));
+
+                visited.remove(&next_junction);
+            }
+        }
+
+        visited.remove(&junction);
+    }
+}
+
+fn get_next_junction(
+    map: &Map,
+    end: &Position,
+    position: Position,
+    distance: usize,
+    visited: &mut HashSet<Position>,
+    graph: &Graph,
+) -> Option<(Position, usize)> {
+    if position.0 == end.0 && position.1 == end.1 {
+        return Some((end.clone(), distance));
+    }
+
+    if graph.contains_key(&position) {
+        return Some((position, distance));
+    }
+
+    visited.insert(position);
+
+    let next_steps = next_steps(map, &position, visited, false);
+
+    match next_steps.len() {
+        0 => None,
+        1 => {
+            let &(_, position) = next_steps.first().unwrap();
+            get_next_junction(map, end, position, distance + 1, visited, graph)
+        }
+        _ => Some((position, distance)),
+    }
+}
+
+fn find_longest_graph_path(
+    graph: &Graph,
+    end: &Position,
+    position: Position,
+    mut visited: HashSet<Position>,
+) -> Option<usize> {
+    if position == *end {
+        return Some(0);
+    }
+
+    visited.insert(position);
+
+    graph[&position]
+        .iter()
+        .filter_map(|(position, distance)| {
+            if visited.contains(position) {
+                return None;
+            }
+            let visited = visited.clone();
+            find_longest_graph_path(graph, end, *position, visited).map(|e| e + distance)
+        })
+        .max()
+}
+
+fn next_steps(
+    map: &Map,
+    position: &Position,
+    visited: &HashSet<Position>,
+    compare_direction: bool,
+) -> Vec<(Direction, Position)> {
     let mut result = vec![];
 
     if position.0 > 0 {
@@ -82,6 +184,7 @@ fn next_steps(map: &Map, position: &Position, visited: &HashSet<Position>) -> Ve
             visited,
             &mut result,
             (position.0 - 1, position.1),
+            compare_direction,
             Direction::Up,
         );
     }
@@ -92,6 +195,7 @@ fn next_steps(map: &Map, position: &Position, visited: &HashSet<Position>) -> Ve
             visited,
             &mut result,
             (position.0, position.1 + 1),
+            compare_direction,
             Direction::Right,
         );
     }
@@ -102,6 +206,7 @@ fn next_steps(map: &Map, position: &Position, visited: &HashSet<Position>) -> Ve
             visited,
             &mut result,
             (position.0 + 1, position.1),
+            compare_direction,
             Direction::Down,
         );
     }
@@ -112,6 +217,7 @@ fn next_steps(map: &Map, position: &Position, visited: &HashSet<Position>) -> Ve
             visited,
             &mut result,
             (position.0, position.1 - 1),
+            compare_direction,
             Direction::Left,
         );
     }
@@ -122,11 +228,12 @@ fn next_steps(map: &Map, position: &Position, visited: &HashSet<Position>) -> Ve
 fn add_position(
     map: &Map,
     visited: &HashSet<Position>,
-    next_positions: &mut Vec<Position>,
+    next_positions: &mut Vec<(Direction, Position)>,
     position: Position,
+    compare_direction: bool,
     direction: Direction,
 ) {
-    if !can_go_to(&map[position.0][position.1], direction) {
+    if !can_go_to(&map[position.0][position.1], compare_direction, direction) {
         return;
     }
 
@@ -134,14 +241,14 @@ fn add_position(
         return;
     }
 
-    next_positions.push(position)
+    next_positions.push((direction, position))
 }
 
-fn can_go_to(node: &Node, from_direction: Direction) -> bool {
+fn can_go_to(node: &Node, compare_direction: bool, from_direction: Direction) -> bool {
     match node {
         Node::Wall => false,
         Node::Empty => true,
-        Node::Slope(slope_direction) => *slope_direction == from_direction,
+        Node::Slope(slope_direction) => !compare_direction || *slope_direction == from_direction,
     }
 }
 
@@ -166,7 +273,7 @@ impl From<char> for Node {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum Direction {
     Up,
     Right,
